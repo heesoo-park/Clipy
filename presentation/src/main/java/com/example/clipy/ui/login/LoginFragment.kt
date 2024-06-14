3c50042be0b1766625abb4be0300b3b61970440a
heesoo <phs8505@gmail.com>
2024-06-14 17:17:22 +0900

    Layout & Feat : 로그인 화면 구성 및 이메일 로그인 기능 초기 구현
    fragment_login.xml에서 지정되어있지 않던 id 지정해주고 로그인 기능흐름을 구현해봄
    
    로그인 페이지 레이아웃은 변경할 예정이고 로그인 기능도 클린 아키텍처에 맞춰 다시 구성할 예정

--- presentation/src/main/java/com/example/clipy/ui/login/LoginFragment.kt
+++ presentation/src/main/java/com/example/clipy/ui/login/LoginFragment.kt
@@ -5,17 +5,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.example.clipy.R
 import com.example.clipy.databinding.FragmentLoginBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class LoginFragment : DialogFragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        auth = Firebase.auth
         arguments?.let {
         }
     }
@@ -32,9 +40,47 @@ class LoginFragment : DialogFragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvWithEmailLogin.setOnClickListener {
+            withEmailLogin("exampleEmail@abc.com", "examplePassword")
+        }
+
+        binding.tvSignupLogin.setOnClickListener {
+            signup("exampleEmail@abc.com", "examplePassword")
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun withEmailLogin(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                dismiss()
+            } else {
+                Toast.makeText(requireContext(), task.exception?.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun signup(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    //Firebase DB에 저장 되어 있는 계정이 아닐 경우
+                    //입력한 계정을 새로 등록한다
+                    Toast.makeText(requireContext(), "signup success", Toast.LENGTH_LONG).show()
+                } else if (task.exception?.message.isNullOrEmpty()) {
+                    Toast.makeText(requireContext(), task.exception?.message, Toast.LENGTH_LONG).show()
+                } else {
+                    //입력한 계정 정보가 이미 Firebase DB에 있는 경우
+                    Toast.makeText(requireContext(), "can't use this email and password", Toast.LENGTH_LONG).show()
+                }
+            }
     }
 
     companion object {
